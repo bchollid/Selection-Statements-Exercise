0b55fe53d5862c233d80785bc3e595d8d4a4b69b
bchollid <brandonholliday1@gmail.com>
2023-12-29 13:26:39 -0700

    Updated Assignment

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -6,27 +6,30 @@
         {
             Console.WriteLine("Welcome to the number guessing game.");
             Console.WriteLine("Try your luck. How big of a number would you like to try to guess?");
-
             var range = int.Parse(Console.ReadLine());
             var r = new Random();
             var favNumber = r.Next(1, range);
+            int userInput;
+            do
+            {
 
-            Console.WriteLine($"Ok, you're going to guess a number between 1 and {range}. Enter your guess now: ");
+                Console.WriteLine($"Ok, you're going to guess a number between 1 and {range}. Enter your guess now: ");
 
-            var userInput = int.Parse(Console.ReadLine());
-          
-            if(userInput < favNumber)
-            {
-                Console.WriteLine($"Sorry. Your guess was too small. The correct number was {favNumber}.");
-            }
-            else if (userInput>favNumber)
-            {
-                Console.WriteLine($"Sorry. Your guess was too big. The correct number was {favNumber}");
-            }
-            else
-            {
-                Console.WriteLine($"You guessed it! It was {favNumber}");
-            }
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine($"Sorry. Your guess was too small.");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine($"Sorry. Your guess was too big.");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it! It was {favNumber}");
+                }
+            } while (favNumber != userInput);
         }
     }
 }
